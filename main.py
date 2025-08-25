58e209bfb127a58f86c959742d8870ecc77b20c0
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-08-25 15:53:01 -0600

    SQL command executed table products created.

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import sqlite3
 
 conn = sqlite3.connect('inventory.db')
 
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS products (
+               id INTEGER,
+               name TEXT,
+               stock INTEGER,
+               price REAL
+               )""")
+
+conn.commit()
+
+conn.close()
