a9064359772f226e652ed9f524e3629250193697
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-08-26 17:42:25 -0600

    Utility Methods

--- operations.py
+++ operations.py
@@ -22,29 +22,36 @@ class DatabaseManager:
         )""")
         self.conn.commit()
 
+    #Utility Methods
     def add_item(self, item: Inventory):
         self.cursor.execute("INSERT OR IGNORE INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
                             (item.id, item.name, item.stock, item.price, item.supplier, item.location,
                              item.weight, item.box_dimensions, item.serial_number, item.manufacture_price))
         self.conn.commit()
 
+    def get_all_items(self):
+        self.cursor.execute("SELECT * FROM products")
+        rows = self.cursor.fetchall()
+        return [Inventory(*row) for row in rows]
 
+    def get_item_by_serial(self, serial_number):
+        self.cursor.execute("SELECT * FROM products WHERE serial_number=?", (serial_number,))
+        row = self.cursor.fetchone()
+        return Inventory(*row) if row else None
 
+    def delete_item(self, item_id):
+        self.cursor.execute("DELETE FROM products WHERE id=?", (item_id,))
+        self.conn.commit()
 
+    def update_stock(self, item_id, new_stock):
+        self.cursor.execute("UPDATE products SET stock=? WHERE id=?", (new_stock, item_id))
+        self.conn.commit()
 
+    def close(self):
+        self.conn.close()
 
-
-
-
-#Utility Methods
-def update_stock(self, new_stock):
-    self.stock = new_stock
-
-def update_price(self, new_price):
-    self.price = new_price
-
-def profit_margin(self, cost_price):
-    return ((self.price - cost_price) / self.price) * 100
-    
-def make_sku(self):
-    return f"{self.name[:3].upper()}-{self.id}-{self.serial_number[-4:]}"
+    def profit_margin(self, cost_price):
+        return ((self.price - cost_price) / self.price) * 100
+        
+    def get_sku(self):
+        return f"{self.name[:3].upper()}-{self.id}-{self.serial_number[-4:]}"
