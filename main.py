b0f0d89eef91e905e2e0efc9bed0f3f069a0c138
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-08-26 17:27:44 -0600

    Bug fix with datbase insertion

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ item_3 = Inventory(3, "Jeans", 75, 49.99, "YSupply", "District 1", 12.5, "15x20x
 
 
 #Add Items to database
-cursor.execute("INSERT INTO OR IGNORE products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+cursor.execute("INSERT OR IGNORE INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
                (item_1.id, item_1.name, item_1.stock, item_1.price, item_1.supplier, item_1.location,
                 item_1.weight, item_1.box_dimensions, item_1.serial_number, item_1.manufacture_price))
-cursor.execute("INSERT INTO OR IGNORE products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+cursor.execute("INSERT OR IGNORE INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                (item_2.id, item_2.name, item_2.stock, item_2.price, item_2.supplier, item_2.location,
                 item_2.weight, item_2.box_dimensions, item_2.serial_number, item_2.manufacture_price))
-cursor.execute("INSERT INTO OR IGNORE products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+cursor.execute("INSERT OR IGNORE INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                (item_3.id, item_3.name, item_3.stock, item_3.price, item_3.supplier, item_3.location,
                 item_3.weight, item_3.box_dimensions, item_3.serial_number, item_3.manufacture_price))
 
