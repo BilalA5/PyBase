e5fccbd7f74ae6881b61ecd614f312138e6c8ddb
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-08-28 13:18:03 -0600

    Table Frame & Scrollbar

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 from inventory import Inventory
-from database import DatabaseManager
+from operations import DatabaseManager
 
 class InventoryGUI:
     def __init__(self, root):
@@ -28,3 +28,22 @@ class InventoryGUI:
             pady=10
         )
         title.pack(fill=tk.X)
+
+        # ====== TABLE FRAME ======
+        table_frame = tk.Frame(self.root, padx=10, pady=10)
+        table_frame.pack(fill=tk.BOTH, expand=True)
+
+        # Product Table
+        columns = ("ID", "Name", "Stock", "Price", "Supplier", "Location", "Weight", "Box Dimensions", "Serial Number", "Manufacture Price")
+        self.product_table = ttk.Treeview(table_frame, columns=columns, show="headings", height=15)
+
+        for col in columns:
+            self.product_table.heading(col, text=col)
+            self.product_table.column(col, width=120, anchor="center")
+
+        self.product_table.pack(side=tk.LEFT, fill=tk.BOTH)
+
+        # Scrollbar
+        scrollbar = ttk.Scrollbar(table_frame, orient=tk.VERTICAL, command=self.product_table.yview)
+        self.product_table.configure(yscroll=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
